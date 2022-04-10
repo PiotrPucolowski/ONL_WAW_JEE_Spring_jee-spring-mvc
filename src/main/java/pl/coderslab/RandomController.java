02f45b557ed6a093df2c490cfcfc5f9a28d1fd5b
PiotrPucolowski <p.pucolowski@gmail.com>
2022-04-10 11:46:56 +0200

    Kontroler podstawy zad 1

--- src/main/java/pl/coderslab/RandomController.java
+++ src/main/java/pl/coderslab/RandomController.java
@@ -2,6 +2,7 @@ package pl.coderslab;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -9,12 +10,20 @@ import java.util.Random;
 
 @Controller
 public class RandomController {
-    @RequestMapping(value = "/show-random", produces = "text/plain;charset=UTF-8")
-    @GetMapping("/show-random")
+    private final Random random = new Random();
+    @GetMapping(value = "random/{max}", produces = "text/plain;charset=UTF-8")
+    @ResponseBody
+    public String showRandomWithMax(@PathVariable int max){
+        int radnomNumber = random.nextInt(max) + 1;
+        return " Użytkownik podał wartość " + max + ", wylosowano liczbę: " + radnomNumber;
+    }
+
+    @GetMapping(value = "/show-random", produces = "text/plain;charset=UTF-8")
     @ResponseBody
     public String showRandom(){
-        Random random = new Random();
         int rand = random.nextInt(100);
         return "Wylosowano liczbę:" + rand;
     }
+
+
 }
