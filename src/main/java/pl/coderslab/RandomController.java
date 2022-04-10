6872f3d563d40bc5c93ce7d1e9677eba00c83ef3
PiotrPucolowski <p.pucolowski@gmail.com>
2022-04-10 11:55:52 +0200

    Kontroler podstawy Zad 2.

--- src/main/java/pl/coderslab/RandomController.java
+++ src/main/java/pl/coderslab/RandomController.java
@@ -10,13 +10,23 @@ import java.util.Random;
 
 @Controller
 public class RandomController {
+
     private final Random random = new Random();
+
+    @GetMapping(value = "random/{min}/{max}", produces = "text/plain;charset=UTF-8")
+    @ResponseBody
+    public String showRandomWithMax(@PathVariable int min, @PathVariable int max){
+        int randomNumber = random.nextInt(max - min + 1) + min;
+        return " Użytkownik podał wartość " + min + " i " + max + ", wylosowano liczbę: " + randomNumber;
+    }
+
     @GetMapping(value = "random/{max}", produces = "text/plain;charset=UTF-8")
     @ResponseBody
     public String showRandomWithMax(@PathVariable int max){
         int radnomNumber = random.nextInt(max) + 1;
         return " Użytkownik podał wartość " + max + ", wylosowano liczbę: " + radnomNumber;
     }
+
 
     @GetMapping(value = "/show-random", produces = "text/plain;charset=UTF-8")
     @ResponseBody
