472bda02f39d7cc16c3525ca1fe1a72b562b2be7
PiotrPucolowski <p.pucolowski@gmail.com>
2022-04-10 13:31:23 +0200

    Widoki Zadanie 2

--- src/main/java/pl/coderslab/HelloCOntroller.java
+++ src/main/java/pl/coderslab/HelloCOntroller.java
@@ -1,7 +1,11 @@
 package pl.coderslab;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Controller
 public class HelloCOntroller {
@@ -21,7 +25,18 @@ public class HelloCOntroller {
 
     }
     @GetMapping("/helloview")
-    public String helloView(){
+    public String helloView(Model model){
+        LocalTime actualTime = LocalTime.now();
+        if (actualTime.isAfter(LocalTime.of(8, 0)) && actualTime.isBefore(LocalTime.of(20,0))){
+            //pomiedzy 8 a 20, color black background color white
+            model.addAttribute("color", "black");
+            model.addAttribute("backgroundColor", "white");
+        }else{
+            //noc
+            model.addAttribute("color", "white");
+            model.addAttribute("backgroundColor", "black");
+
+        }
 
         return "home";
     }
