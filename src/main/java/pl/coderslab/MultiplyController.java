9c811dd528f28ee781469c0f94293de4a396d538
PiotrPucolowski <p.pucolowski@gmail.com>
2022-04-10 20:59:12 +0200

    Widoki Zadanie 3-4

---
+++ src/main/java/pl/coderslab/MultiplyController.java
@@ -0,0 +1,23 @@
+package pl.coderslab;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class MultiplyController {
+
+    @GetMapping("/multiply")
+    public String multiply(Model model, @RequestParam(defaultValue = "10") int size) {
+        model.addAttribute("rows", size);
+        model.addAttribute("cols", size);
+        return "multiplicationTable";
+    }
+
+    @GetMapping("/paramMultiply/{rows}/{cols}")
+    public String paramMultiply(Model model, @PathVariable("rows") int rows, @PathVariable("cols") int cols) {
+        model.addAttribute("rows", rows);
+        model.addAttribute("cols", cols);
+        return "multiplicationTable";
+    }
+}
